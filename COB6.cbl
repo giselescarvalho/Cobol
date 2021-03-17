3c9228e00975ce72c66d6a4fd9665ca0e43b96b9
unknown <giseledasilvac@gmail.com>
2021-03-17 08:06:35 -0300

    Quatro operadores aritméticos

---
+++ COB6.cbl
@@ -0,0 +1,47 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. COB5.
+      ******************************************************************
+      * Author: Gisele Carvalho
+      * Date: 16/03/2021
+      * Purpose: Operadores aritiméticos
+      * Tectonics: cobc
+      ******************************************************************
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       77 WRK-NUM1   PIC 9(02) VALUE ZEROS.
+       77 WRK-NUM2   PIC 9(02) VALUE ZEROS.
+       77 WRK-RESUL  PIC 9(04) VALUE ZEROS.
+       77 WRK-RESTO  PIC 9(02) VALUE ZEROS.
+       PROCEDURE DIVISION.
+           ACCEPT WRK-NUM1 FROM CONSOLE.
+           ACCEPT WRK-NUM2 FROM CONSOLE.
+           DISPLAY  '---------------------'.
+           DISPLAY 'PRIMEIRO NUMERO : ' WRK-NUM1.
+           DISPLAY 'SEGUNDO NUMERO : '  WRK-NUM2.
+      *-----------------*
+      *       SOMA      *
+      *-----------------*
+           DISPLAY  '--------- SOMA ------------'.
+           ADD WRK-NUM1 WRK-NUM2 TO WRK-RESUL.
+            DISPLAY 'ADICAO : ' WRK-RESUL.
+      *-----------------*
+      *     SUBTRACAO   *
+      *-----------------*
+           DISPLAY  '--------- SUBTRACAO ------------'.
+           SUBTRACT WRK-NUM1 FROM WRK-NUM2 GIVING WRK-RESUL.
+            DISPLAY 'SUBTRACAO : ' WRK-RESUL.
+      *-----------------*
+      *  MULTIPLICACAO  *
+      *-----------------*
+           MULTIPLY WRK-NUM1 BY WRK-NUM2 GIVING WRK-RESUL.
+            DISPLAY 'MULTIPLICACAO : ' WRK-RESUL.
+      *-----------------*
+      *  DIVISAO  *
+      *-----------------*
+           DIVIDE WRK-NUM1 BY WRK-NUM2 GIVING WRK-RESUL
+            REMAINDER WRK-RESTO.
+            DISPLAY 'DIVISAO : ' WRK-RESUL.
+            DISPLAY 'RESTO : ' WRK-RESTO.
+           STOP RUN.
+       END PROGRAM COB5.
