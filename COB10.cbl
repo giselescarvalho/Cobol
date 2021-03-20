41b2b45e19f1801359e4aab631c960ab1a9d9c5e
unknown <giseledasilvac@gmail.com>
2021-03-20 12:31:05 -0300

    Frete Condicional

--- COB10.cbl
+++ COB10.cbl
@@ -12,13 +12,13 @@
            DECIMAL-POINT IS COMMA.
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       77 WRK-PRODUTO PIC X(20) VALUE SPACES.
+       77 WRK-PRODUTO PIC X(20)    VALUE SPACES.
        77 WRK-VALOR   PIC 9(06)V99 VALUE ZEROS.
-       77 WRK-UF      PIC X(02) VALUE SPACES.
+       77 WRK-UF      PIC X(02)    VALUE 'SP'.
        77 WRK-FRETE   PIC 9(06)V99 VALUE ZEROS.
        PROCEDURE DIVISION.
            DISPLAY  'Digite Produto'
-           ACCEPT WRK-PRODUTO FROM CONSOLE.
+             ACCEPT WRK-PRODUTO FROM CONSOLE.
            DISPLAY  'Digite Valor do Produto'
            ACCEPT WRK-VALOR FROM CONSOLE.
            DISPLAY  'Digite UF com dois digitos'
