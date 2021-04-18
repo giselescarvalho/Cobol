c7f6c979a668af44fd51e09f4b4a452a621b60fa
unknown <giseledasilvac@gmail.com>
2021-04-18 20:30:26 -0300

    Alterando registros

--- CLIENTES.cbl
+++ CLIENTES.cbl
@@ -2,9 +2,9 @@
        PROGRAM-ID. CLIENTES.
       ******************************************************************
       * Author: Gisele Carvalho
-      * Date: 23/03/2021
+      * Date: 18/04/2021
       * Purpose: Cobol parte 2: Alura - Manutenção de Registros
-      *           Consultando registros
+      *           Alterando registros
       *           Modo Batch - diretamente acessado
       * Tectonics: cobc
       ******************************************************************
@@ -12,11 +12,11 @@
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
            SELECT CLIENTES
-             ASSIGN TO 'C:\Users\Gisele\Desktop\Cobol\CLIENTE.DAT'
-             ORGANIZATION IS INDEXED
-             ACCESS MODE IS RANDOM
-             FILE STATUS IS CLIENTES-STATUS
-             RECORD KEY IS  CLIENTES-CHAVE.
+             ASSIGN TO 'C:\Users\Gisele\Desktop\Cobol\CLIENTES.DAT'
+               ORGANIZATION IS INDEXED
+               ACCESS MODE IS RANDOM
+               FILE STATUS IS CLIENTES-STATUS
+               RECORD KEY IS  CLIENTES-CHAVE.
        DATA DIVISION.
        FILE SECTION.
        FD CLIENTES.
@@ -68,10 +68,13 @@
                10 COLUMN PLUS 2 PIC X(40) USING CLIENTES-EMAIL.
        01 MOSTRA-ERRO.
              02 MSG-ERRO.
-               10 LINE 16 COLUMN 01 ERASE EOL BACKGROUND-COLOR 3.
-               10 LINE 16 COLUMN 10 PIC X(30) BACKGROUND-COLOR 3
+               10 LINE 16 COLUMN 01 ERASE EOL
+                             BACKGROUND-COLOR 3.
+               10 LINE 16 COLUMN 10 PIC X(30)
+                             BACKGROUND-COLOR 3
                              FROM WRK-MSGERRO.
-               10 COLUMN PLUS 2 PIC X(01) BACKGROUND-COLOR 3
+               10 COLUMN PLUS 2 PIC X(01)
+                             BACKGROUND-COLOR 3
                              USING WRK-TECLA.
 
        PROCEDURE DIVISION.
@@ -94,15 +97,16 @@
             ACCEPT MENU.
 
        2000-PROCESSAR.
+            MOVE SPACES TO CLIENTES-NOME CLIENTES-EMAIL WRK-MSGERRO.
             EVALUATE WRK-OPCAO
               WHEN 1
                PERFORM 5000-INCLUIR
               WHEN 2
-                CONTINUE
+                PERFORM 6000-CONSULTAR
               WHEN 3
-                CONTINUE
+                PERFORM 7000-ALTERAR
               WHEN 4
-                CONTINUE
+                PERFORM 8000-EXCLUIR
               WHEN 5
                 CONTINUE
               WHEN OTHER
@@ -113,8 +117,11 @@
               PERFORM 1100-MONTATELA.
 
 
+
        3000-FINALIZAR.
              CLOSE CLIENTES.
+
+
 
        5000-INCLUIR.
              MOVE 'MODULO - INCLUSAO ' TO WRK-MODULO.
@@ -139,3 +146,46 @@
                    DISPLAY SS-DADOS
                  END-READ.
                    ACCEPT MOSTRA-ERRO.
+
+       7000-ALTERAR.
+             MOVE 'MODULO - ALTERAR ' TO WRK-MODULO.
+             DISPLAY TELA.
+             DISPLAY TELA-REGISTRO.
+              ACCEPT CHAVE.
+                READ CLIENTES
+                IF CLIENTES-STATUS = 0
+                    ACCEPT SS-DADOS
+                     REWRITE CLIENTES-REG
+                       IF CLIENTES-STATUS = 0
+                            MOVE 'REGISTRO ALTERADO ' TO WRK-MSGERRO
+                            ACCEPT MOSTRA-ERRO
+                       ELSE
+                            MOVE 'REGISTRO NAO ALTERADO' TO WRK-MSGERRO
+                            ACCEPT MOSTRA-ERRO
+                       END-IF
+                 ELSE
+                      MOVE 'REGISTO NAO ENCONTRADO ' TO WRK-MSGERRO
+                      ACCEPT MOSTRA-ERRO
+                END-IF.
+
+
+       8000-EXCLUIR.
+             MOVE 'MODULO - EXCLUSAO ' TO WRK-MODULO.
+             DISPLAY TELA.
+               DISPLAY TELA-REGISTRO.
+               ACCEPT CHAVE.
+                READ CLIENTES
+                  INVALID KEY
+                   MOVE 'NAO ENCONTRADO   '  TO WRK-MSGERRO
+                 NOT INVALID KEY
+                  MOVE ' ENCONTRADO  (S/N) ? '  TO WRK-MSGERRO
+                   DISPLAY SS-DADOS
+                END-READ.
+                  ACCEPT MOSTRA-ERRO.
+                    IF WRK-TECLA = 'S' AND CLIENTES-STATUS = 0
+                           DELETE CLIENTES
+                            INVALID KEY
+                            MOVE 'NAO EXCLUIDO ' TO WRK-MSGERRO
+                            ACCEPT  MOSTRA-ERRO
+                          END-DELETE
+                     END-IF.
